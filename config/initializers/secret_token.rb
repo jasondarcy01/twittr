77e71021f2fb11b37fb30e839aaf1da36557c14d
darchcruise <darchcruise@yahoo.com>
2013-07-15 10:30:26 -0400

    create user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittr::Application.config.secret_token = 'df3b0df1fecdaea4af87dd157498a54487fb12ea43c6593aa00ac336e5e0a21b1988a4e465f7c9dc6d015a739de0a6126392e64f0bf9f40febeb627344bf343f'
